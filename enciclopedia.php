79d770cbfd191984275c2bdbc3b84c4985edb18e
YumeshiSama <alejo.2002@live.com.ar>
2021-10-12 16:05:26 -0300

    enciclopedia

---
+++ enciclopedia.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no, user-scalable=no">
+	<title>Beta_ada</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link href="https://fonts.googleapis.com/css?family=Poppins:400,600&display=swap" rel="stylesheet">
+ 	<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css'>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	<script src="https://kit.fontawesome.com/aa00e73738.js" crossorigin="anonymous"></script>
+</head>
+<body>
+<?php include 'navbar.php'; ?>
+<div class="container">
+
+        <div class="progress_bar_top">
+		        <div class="row">
+		          <div class="col-12 col-lg-8 ml-auto mr-auto">
+		            <div class="multisteps-form__progress">
+		              
+		            </div>
+		          </div>
+		        </div>
+		    </div>
+
+    <div class="card">
+        <div class="card-body row">
+          <h5 class="card-title">Alamo</h5>
+          <div class="row">
+            <img class="col-md-3" width="100px" src="https://via.placeholder.com/100.png/09f/fff" alt="">
+            <p class="col-md-6 card-text">El alamo es una especie que Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum iusto deleniti culpa officia numquam minima, sint pariatur voluptate molestias? Quod vitae adipisci pariatur et vero nobis unde voluptate at corrupti?</p>
+          </div>
+        </div>
+    </div>
+    <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">Pino</h5>
+          <div class="row">
+            <img class="col-md-3" width="100px" src="https://via.placeholder.com/100.png/09f/fff" alt="">
+            <p class="col-md-6 card-text">El pino es una especie que Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum iusto deleniti culpa officia numquam minima, sint pariatur voluptate molestias? Quod vitae adipisci pariatur et vero nobis unde voluptate at corrupti?</p>
+          </div>
+        </div>
+    </div>
+    <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">Sauce</h5>
+          <div class="row">
+            <img class="col-md-3" width="100px" src="https://via.placeholder.com/100.png/09f/fff" alt="">
+            <p class="col-md-6 card-text">El sauce es una especie que Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum iusto deleniti culpa officia numquam minima, sint pariatur voluptate molestias? Quod vitae adipisci pariatur et vero nobis unde voluptate at corrupti?</p>
+          </div>
+        </div>
+    </div>
+
+</div>
+</body>
+</html>
