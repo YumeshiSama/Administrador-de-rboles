f56e0d0e41dd5bbdbfbc59178355284e596264b6
YumeshiSama <alejo.2002@live.com.ar>
2021-10-13 11:51:46 -0300

    muestra las arboledas el putito le encanta mostrar arboledas

--- coordenadas.php
+++ coordenadas.php
@@ -5,31 +5,40 @@ include 'conexion.php';
 if (empty($_GET["categoria"])) {
     exit("No hay categoría");
 }
-
 $categoria = $_GET["categoria"];
+//$categoria="1";
+//$contador=0;
+$arreglo=array();
+$arreglodos=array();
+$var="]";
 
     $sql = "SELECT latitud, longitud FROM arboles WHERE id_especie='1'";
     $result = $conn->query($sql);
 
        while($row = mysqli_fetch_array($result)) {
-
-    $pinos = [
-        [
-            "latitud" => $row["latitud"],
-            "longitud" => $row["longitud"],
-        ],
-    ];
+           
+           $arreglodos =  ["latitud" => $row["latitud"],"longitud" => $row["longitud"]];
+           array_push($arreglo,$arreglodos);
+            //$contador= $contador+1;
+            // $pinos =[
+            //             [
+            //                 "latitud" => $row["latitud"],
+            //                 "longitud" => $row["longitud"],
+            //             ],
+            //         ];
+            
 }
+
 
 if ($categoria === "1") {
     echo json_encode([
         "icono" => "img/pino.png",
-        "coordenadas" => $pinos,
+        "coordenadas" => $arreglo,
     ]);
 } else {
     echo json_encode([
         "icono" => "img/alamo.png",
-        "coordenadas" => $alamos,
+        "coordenadas" => $arreglo,
     ]);
 }
 
