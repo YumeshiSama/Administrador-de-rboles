3c7492c7b99a5ec1d8cbb7af09346c812a2dbb3a
YumeshiSama <alejo.2002@live.com.ar>
2021-10-13 18:19:59 -0300

    conexion arreglada

--- conexion.php
+++ conexion.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$database = "ada";
-$username = "root";
-$password = "";
+$servername = "mysql.hostinger.com.ar";
+$database = "u878650187_ada";
+$username = "u878650187_ada";
+$password = "Abc.12345678";
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $database);
 
