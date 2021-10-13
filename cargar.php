204183aa722264793e6f5c0960be647fd439797b
Zekken02 <nicopiris123@hotmail.com>
2021-10-13 19:37:23 -0300

    fixes jiji

--- cargar.php
+++ cargar.php
@@ -18,9 +18,9 @@ $foto_tronco=0;
 $foto_copa=0;
 $id_usuario=0;
 
-//$sql = "INSERT INTO `arboles` (`id`, `id_usuario`, `id_edad`, `id_magnitud`, `extraible`, `poda`, `peligro_de_caida`, `id_copa`, `id_salud`, `id_localidad`, `id_especie`, `latitud`, `longitud`, `fecha_carga`, `foto_tronco`, `foto_copa`, `distancia_prox`, `comentario`) VALUES (NULL, '$id_usuario', '$edad', '$magnitud', '$extraccion', '$poda', '$caida', '$copa', '$salud', '0', '$especie', '$latitud', '$longitud', '$date', '$foto_tronco', '$foto_copa', '$distancia', '$comentario')";
+$sql = "INSERT INTO `arboles` (`id`, `id_usuario`, `id_edad`, `id_magnitud`, `extraible`, `poda`, `peligro_de_caida`, `id_copa`, `id_salud`, `id_localidad`, `id_especie`, `latitud`, `longitud`, `fecha_carga`, `foto_tronco`, `foto_copa`, `distancia_prox`, `comentario`) VALUES (NULL, '$id_usuario', '$edad', '$magnitud', '$extraccion', '$poda', '$caida', '$copa', '$salud', '0', '$especie', '$latitud', '$longitud', '$date', '$foto_tronco', '$foto_copa', '$distancia', '$comentario')";
 
-$sql = "INSERT INTO `arboles` (`id`, `id_usuario`, `id_edad`, `id_extraccion`, `id_magnitud`, `id_columnar`, `id_copa`, `id_salud`, `id_localidad`, `id_especie`, `latitud`, `longitud`, `fecha_carga`, `foto_tronco`, `foto_copa`, `distancia_prox`, `comentario`) VALUES (NULL, '$id_usuario', '$edad', '$extraccion', '$magnitud', '0', '$copa', '$salud', '0', '$especie', '$latitud', '$longitud', '$date', '$foto_tronco', '$foto_copa', '$distancia', '$comentario')";
+//$sql = "INSERT INTO `arboles` (`id`, `id_usuario`, `id_edad`, `id_extraccion`, `id_magnitud`, `id_columnar`, `id_copa`, `id_salud`, `id_localidad`, `id_especie`, `latitud`, `longitud`, `fecha_carga`, `foto_tronco`, `foto_copa`, `distancia_prox`, `comentario`) VALUES (NULL, '$id_usuario', '$edad', '$extraccion', '$magnitud', '0', '$copa', '$salud', '0', '$especie', '$latitud', '$longitud', '$date', '$foto_tronco', '$foto_copa', '$distancia', '$comentario')";
 
 mysqli_query($conn,$sql);
 
