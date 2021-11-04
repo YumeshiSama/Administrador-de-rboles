f83f3b32a41191a391924e25ad2b208ed328575b
YumeshiSama <alejo.2002@live.com.ar>
2021-11-03 22:33:36 -0300

    lista users

---
+++ moduloeliminar.php
@@ -0,0 +1,28 @@
+<?php 
+include 'conexion.php';
+$id = $_REQUEST['id'];
+$sql = "DELETE FROM `usuarios` WHERE id= $id";
+
+if ($conn->query($sql) === TRUE) {
+  echo "Record deleted successfully";
+} else {
+  echo "Error deleting record: " . $conn->error;
+};
+
+if ($query_does_not_execute) {
+    $errcode = "Hubo un error al eliminar";
+}
+
+$referer = $_SERVER['HTTP_REFERER'];
+
+if ($errcode) {
+    if (strpos($referer, '?') === false) {
+        $referer .= "?";
+    }
+
+    header("Location: $referer&$errcode");
+} else {
+    header("Location: $referer");
+}
+exit;
+?>
