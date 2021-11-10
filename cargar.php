947cb67d215f50b9cbccefced9771a7440d4929c
YumeshiSama <alejo.2002@live.com.ar>
2021-11-10 16:23:41 -0300

    Cargar especie con img

--- cargar.php
+++ cargar.php
@@ -1,6 +1,9 @@
 <?php
 session_start();
-
+if (empty($_SESSION["id_usuario"])) {
+    header("Location: index.php");
+    exit();
+};
 include "conexion.php";
 $id_usuario=$_SESSION["id_usuario"];
 $edad=$_REQUEST["edad"];
@@ -18,7 +21,7 @@ $date = date("Y-m-d");
 $datename = date("Y-m-d-s-v");
 $comentario = $_REQUEST["detalles_extraccion"];
 
-    $targetDir = "uploads/";
+    $targetDir = "upl/img/";
 
 	$fileNameCopa = basename($_FILES["copa"]["name"]);
 	$fileNameCopa=$datename.$fileNameCopa;
@@ -36,7 +39,7 @@ $comentario = $_REQUEST["detalles_extraccion"];
 		if ((!file_exists($targetFilePathCopa)) && (!file_exists($targetFilePathTronco))) {
 			if((in_array($fileTypeCopa, $allowTypes)) && (in_array($fileTypeTronco, $allowTypes))){
 				if((move_uploaded_file($_FILES["copa"]["tmp_name"], $targetFilePathCopa)) && (move_uploaded_file($_FILES["tronco"]["tmp_name"], $targetFilePathTronco))){		
-					$sql = "INSERT INTO `arboles` (`id`, `id_usuario`, `id_edad`, `id_magnitud`, `extraible`, `poda`, `peligro_de_caida`, `id_copa`, `id_salud`, `id_localidad`, `id_especie`, `latitud`, `longitud`, `fecha_carga`, `foto_tronco`, `foto_copa`, `distancia_prox`, `comentario`) VALUES (NULL, '$id_usuario', '$edad', '$magnitud', '$extraccion', '$poda', '$caida', '$copa', '$salud', '0', '$especie', '$latitud', '$longitud', '$date', 'uploads/$fileNameTronco', 'uploads/$fileNameCopa', '$distancia', '$comentario')";
+					$sql = "INSERT INTO `arboles` (`id`, `id_usuario`, `id_edad`, `id_magnitud`, `extraible`, `poda`, `peligro_de_caida`, `id_copa`, `id_salud`, `id_localidad`, `id_especie`, `latitud`, `longitud`, `fecha_carga`, `foto_tronco`, `foto_copa`, `distancia_prox`, `comentario`) VALUES (NULL, '$id_usuario', '$edad', '$magnitud', '$extraccion', '$poda', '$caida', '$copa', '$salud', '0', '$especie', '$latitud', '$longitud', '$date', 'upl/img/$fileNameTronco', 'upl/img/$fileNameCopa', '$distancia', '$comentario')";
                     mysqli_query($conn,$sql);
                 }
 			}
--- nueva_categoria.php
+++ nueva_categoria.php
@@ -6,12 +6,38 @@ if (empty($_SESSION["id_usuario"])) {
 };
 include 'conexion.php';
 
+$datename = date("Y-m-d-s-v");
+$targetDir = "upl/icn/";
+
 if(isset($_REQUEST['cargar'])) // when click on Update button
 {
     $nombre = $_REQUEST['nombre'];
-    $icon = $_REQUEST['icon'];
-	$consulta ="INSERT INTO `especies` (`id`, `nombre_especie`, `imagen`) VALUES (NULL, '$nombre', '$icon')";
-    $qry = mysqli_query($conn, $consulta);
+
+    $fileName = basename($_FILES["icon"]["name"]);
+	$fileName=$datename.$fileName;
+	$targetFilePath = $targetDir . $fileName;
+	$fileType = pathinfo($targetFilePath,PATHINFO_EXTENSION);
+
+	if((!empty($_FILES["icon"]["name"]))){
+		// Allow certain file formats
+		$allowTypes = array('jpg','png','jpeg');
+		if ((!file_exists($targetFilePath))) {
+			if((in_array($fileType, $allowTypes))){
+				if((move_uploaded_file($_FILES["icon"]["tmp_name"], $targetFilePath))){	
+					$consulta ="INSERT INTO `especie` (`id`, `nombre_especie`, `imagen`) VALUES (NULL, '$nombre', 'upl/icn/$fileName')";
+                    $qry = mysqli_query($conn, $consulta);
+                }
+			}
+		}
+	}
+
+
+
+
+    
+    //$icon = $_REQUEST['icon'];
+	// $consulta ="INSERT INTO `especies` (`id`, `nombre_especie`, `imagen`) VALUES (NULL, '$nombre', '$icon')";
+    // $qry = mysqli_query($conn, $consulta);
 	
     header("Location: usuarios_lista.php");
 
@@ -20,7 +46,7 @@ exit;
 }
 ?>
 
-<form method="POST">
+<form class="multisteps-form__form" method="post" enctype="multipart/form-data">
     <label for="nombre">Nombre de la especie</label>
     <input type="text" name="nombre"  Required>
     <div class="col-3 fileUpload btn btn-primary">
