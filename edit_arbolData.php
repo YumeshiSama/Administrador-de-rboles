585d5ead34a4396f1f5f520aeaa3baff84a5407c
Zekken02 <nicopiris123@hotmail.com>
2021-11-16 01:29:13 -0300

    update

--- edit_arbolData.php
+++ edit_arbolData.php
@@ -7,31 +7,36 @@ $type=$_REQUEST["type"];
 switch ($type) {
     case 1:
         $especie=$_REQUEST["especie"];
-        $sql = "UPDATE `arboles` SET `id_especie` = `$especie`  where `id`=`$id`";
+        $sql = "UPDATE arboles SET id_especie = $especie  where id=$id";
         mysqli_query($conn, $sql);
         break;
     case 2:
         $edad=$_REQUEST["edad"];
-        echo $edad;
+        $sql = "UPDATE arboles SET id_edad = $edad  where id=$id";
+        mysqli_query($conn, $sql);
         break;
     case 3:
         $magnitud=$_REQUEST["magnitud"];
-        echo $magnitud;
+        $sql = "UPDATE arboles SET id_magnitud = $magnitud  where id=$id";
+        mysqli_query($conn, $sql);
         break;
     case 4:
         $copa=$_REQUEST["copa"];
-        echo $copa;
+        $sql = "UPDATE arboles SET id_copa = $copa  where id=$id";
+        mysqli_query($conn, $sql);
         break;
     case 5:
         $salud=$_REQUEST["salud"];
-        echo $salud;
+        $sql = "UPDATE arboles SET id_salud = $salud  where id=$id";
+        mysqli_query($conn, $sql);
         break;
     case 6:
         $comentario=$_REQUEST["comentario"];
-        echo $comentario;
+        $sql = "UPDATE arboles SET comentario = $comentario  where id=$id";
+        mysqli_query($conn, $sql);
         break;
 }
 
-// header("Location: index.php");
+header("Location: index.php");
 
-// exit;
+exit;
